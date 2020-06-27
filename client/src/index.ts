e8a44483a11ff45ccc5fd69b6e6a05347d27ac64
--global <haoy2001@gmail.com>
2020-06-27 02:29:45 -0400

    Basic multiplayer

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,9 +1,69 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // import * as Howler from "howler";
-// import * as Colyseus from "colyseus.js";
+import * as Colyseus from "colyseus.js";
 import * as Kontra from "kontra";
 import { MirroredRepeatWrapping } from "three";
+
+Kontra.init(document.createElement("canvas"));
+Kontra.initKeys();
+
+let loop = Kontra.GameLoop({
+  update: update,
+  render: render,
+});
+
+var client = new Colyseus.Client("ws://localhost:2567");
+var room: Colyseus.Room;
+
+var players: { [sessionId: string]: THREE.Mesh } = {};
+
+function newCube() {
+  var geometry = new THREE.SphereBufferGeometry(1, 20, 20);
+  var material = new THREE.MeshPhongMaterial({
+    color: "purple",
+    reflectivity: 1,
+    shininess: 140,
+  });
+
+  var cube = new THREE.Mesh(geometry, material);
+  cube.position.set(0, 1.7 / 2, 0);
+  return cube;
+}
+
+client.joinOrCreate("game_room").then((init_room) => {
+  console.log("connected");
+  room = init_room;
+  console.log(room.send);
+
+  // room.send("test", {});
+
+  room.state.players.onAdd = (player: any, sessionId: string) => {
+    console.log("Joined", player, sessionId);
+    players[sessionId] = newCube();
+    scene.add(players[sessionId]);
+  };
+
+  room.state.players.onRemove = (player: any, sessionId: string) => {
+    console.log("Left", player, sessionId);
+    scene.remove(players[sessionId]);
+    delete players[sessionId];
+  };
+
+  room.state.players.onChange = (player: any, sessionId: string) => {
+    console.log(player);
+    players[sessionId].position.set(player.x, player.y, player.z);
+  };
+
+  loop.start();
+
+  window.addEventListener("keydown", function (e) {
+    if (e.which == 38) {
+      room.send("move", { y: -1 });
+      console.log("moving");
+    }
+  });
+});
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
@@ -20,16 +80,6 @@ document.body.appendChild(renderer.domElement);
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-
-var geometry = new THREE.SphereBufferGeometry(1, 20, 20);
-var material = new THREE.MeshPhongMaterial({
-  color: 'purple',
-  reflectivity: 1,
-  shininess: 140
-});
-var cube = new THREE.Mesh(geometry, material);
-cube.position.set(0, 1.7/2, 0);
-scene.add(cube);
 
 const ground_geo = new THREE.PlaneBufferGeometry(1000, 1000);
 const ground_tex = new THREE.TextureLoader().load(
@@ -69,6 +119,9 @@ spotLight.shadow.camera.near = 10;
 spotLight.shadow.camera.far = 200;
 scene.add(spotLight);
 
+var cube = newCube();
+scene.add(cube);
+
 const loader = new THREE.CubeTextureLoader();
 const texture = loader.load([
   require("../skybox/pos-x.png").default,
@@ -78,38 +131,27 @@ const texture = loader.load([
   require("../skybox/pos-z.png").default,
   require("../skybox/neg-z.png").default,
 ]);
-texture;
 scene.background = texture;
-
-Kontra.init(document.createElement("canvas"));
-Kontra.initKeys();
-
-let loop = Kontra.GameLoop({
-  update: update,
-  render: render,
-});
-
-loop.start();
 
 const direction = new THREE.Vector3();
 
 function update(dt: number) {
-  const speed = 0.1;
   if (Kontra.keyPressed("a")) {
-    cube.position.x -= speed;
-  } else if (Kontra.keyPressed("d")) {
-    cube.position.x += speed;
-  } else if (Kontra.keyPressed("w")) {
-    cube.position.z -= speed;
-  } else if (Kontra.keyPressed("s")) {
-    cube.position.z += speed;
+    room.send("move", { x: -0.1 });
+  }
+  if (Kontra.keyPressed("d")) {
+    room.send("move", { x: 0.1 });
+  }
+  if (Kontra.keyPressed("w")) {
+    room.send("move", { z: -0.1 });
+  }
+  if (Kontra.keyPressed("s")) {
+    room.send("move", { z: 0.1 });
   }
 
   cube.getWorldPosition(controls.target);
   controls.update();
-
   // update the transformation of the camera so it has an offset position to the current target
-
   direction.subVectors(camera.position, controls.target);
   direction.normalize().multiplyScalar(10);
   camera.position.copy(direction.add(controls.target));
