4a09461dbdaf1e4c61e3c885500114f2bb17d55d
Howard Yun <haoy2001@gmail.com>
2020-08-03 02:19:49 -0400

    Basic world

--- client/src/graphics/World.ts
+++ client/src/graphics/World.ts
@@ -4,45 +4,46 @@ import { Player } from "./Player";
 export class World {
   players: { [sessionId: string]: Player } = {};
   scene = new THREE.Scene();
-  private axesHelper = new THREE.AxesHelper(10);
-  private ground_tex = new THREE.TextureLoader().load(
-    require("../../res/dirt.png").default
-  );
-  private ground_geo = new THREE.PlaneBufferGeometry(1000, 1000);
   private ground: THREE.Mesh;
-  private spotLight = new THREE.SpotLight();
+  private dirLight: THREE.DirectionalLight;
 
   constructor() {
-    this.scene.add(this.axesHelper);
+    this.scene.add(new THREE.AxesHelper(10));
+    this.scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+
+    // sun
+    this.dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    this.dirLight.color.setHSL(0.1, 1, 0.95);
+    this.dirLight.position.set(-500, 40, -30);
+    this.dirLight.castShadow = true;
+
+    var d = 250;
+    this.dirLight.shadow.mapSize.width = 4096;
+    this.dirLight.shadow.mapSize.height = 4096;
+    this.dirLight.shadow.camera.left = -d;
+    this.dirLight.shadow.camera.right = d;
+    this.dirLight.shadow.camera.top = d;
+    this.dirLight.shadow.camera.bottom = -d;
+    this.dirLight.shadow.camera.far = 3000;
+    this.scene.add(this.dirLight);
+    this.scene.add(new THREE.DirectionalLightHelper(this.dirLight, 20));
 
     // ground
-    this.ground_tex.repeat.set(1000, 1000);
-    this.ground_tex.wrapS = THREE.RepeatWrapping;
-    this.ground_tex.wrapT = THREE.RepeatWrapping;
+    const ground_tex = new THREE.TextureLoader().load(
+      require("../../res/dirt.png").default
+    );
+    ground_tex.repeat.set(1000, 1000);
+    ground_tex.wrapS = THREE.RepeatWrapping;
+    ground_tex.wrapT = THREE.RepeatWrapping;
     const ground_mat = new THREE.MeshPhongMaterial({
-      map: this.ground_tex,
+      map: ground_tex,
       dithering: true,
     });
-    this.ground = new THREE.Mesh(this.ground_geo, ground_mat);
+    const ground_geo = new THREE.PlaneBufferGeometry(1000, 1000);
+    this.ground = new THREE.Mesh(ground_geo, ground_mat);
     this.ground.rotation.x = -Math.PI / 2;
+    this.ground.receiveShadow = true;
     this.scene.add(this.ground);
-
-    // ambient light
-    const ambient = new THREE.AmbientLight(0xffffff, 0.3);
-    this.scene.add(ambient);
-
-    // spotlight
-    this.spotLight.position.set(2, 10, 2);
-    this.spotLight.angle = Math.PI / 4;
-    this.spotLight.penumbra = 0.2;
-    this.spotLight.decay = 4;
-    this.spotLight.distance = 100;
-    this.spotLight.castShadow = true;
-    this.spotLight.shadow.mapSize.width = 1024;
-    this.spotLight.shadow.mapSize.height = 1024;
-    this.spotLight.shadow.camera.near = 10;
-    this.spotLight.shadow.camera.far = 200;
-    this.scene.add(this.spotLight);
 
     // skybox
     const loader = new THREE.CubeTextureLoader();
